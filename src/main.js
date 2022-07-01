3eb80de3b6a2e9a4cc445c5d1e0d48730a09895e
liuweinb <2787601360@qq.com>
2022-07-01 23:17:20 +0800

    init

---
+++ src/main.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import router from './router/index'
+import store from './store/index'
+
+
+import { Button,Stepper } from 'vant';
+Vue.use(Button)
+Vue.use(Stepper)
+
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    这是home页面
+    <van-button type="primary">主要按钮</van-button>
+    <van-stepper v-model="value" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'home',
+  data(){
+    return{
+      value:1
+    }
+  }
+}
+</script>
